6b2a32cd97dd709f8a1e15feda2c98f661b9d1f7
KauaHVieira <161729999+KauaHVieira@users.noreply.github.com>
2024-04-17 13:41:27 +0000

    pensamentos sobre a resolucao

---
+++ StartGameimg.js
@@ -0,0 +1,64 @@
+<script> 
+ 
+ 
+ //function desenhaImagens() {
+//canvas = document.getElementById("canv");
+//ctx = canvas.getContext("2d");
+//img = new Image();
+//img.src = "img/aeroinimigaprofinal.png";
+//img.onload = function(){
+//ctx.drawImage(img, 0, 0);
+//ctx.drawImage(img, 600, 0, 100, 100);
+  //   }
+ //}
+
+ var cor = 'blue';
+ var cont = 1;
+ var myGamePiece;
+ function startGame() {
+ myGamePiece = new component(30, 30, "red", 0, 0);
+ myGameArea.start();
+ }
+ 
+ var myGameArea = {
+ canvas : document.createElement("canvas"),
+ start : function() {
+ this.canvas.width = 480;
+ this.canvas.height = 270;
+ this.context = this.canvas.getContext("2d");
+ document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+ this.interval = setInterval(updateGameArea, 20);
+ },
+ clear : function() {
+ this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+ }
+ }
+ 
+ function component(width, height, color, x, y) {
+ this.width = width;
+ this.height = height;
+ this.x = x;
+ this.y = y;
+ this.color;
+ this.update = function(){
+ ctx = myGameArea.context;
+ if(cont % 10 == 0)
+ cor = 'red';
+ else
+ cor = 'blue';
+ //ctx.fillStyle = color;
+ ctx.fillStyle = cor;
+ ctx.fillRect(this.x, this.y, this.width, this.height);
+ cont++;
+ 
+ this.x++
+ this.y++
+ }
+ }
+ 
+ function updateGameArea() {
+ myGameArea.clear();
+ myGamePiece.x += 1;
+ myGamePiece.update();
+ }
+ </script>
